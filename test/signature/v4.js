3f58ffed1e9cad76df64b5001623de499bb53f78
ykokw <ykokw@example.com>
2016-12-07 18:04:24 +0900

    Fixed to set x-nifty-date at first sendRequest method

--- test/signature/v4.js
+++ test/signature/v4.js
@@ -63,5 +63,18 @@ describe("Signature ", ()=>{
         assert.equal(expect, payload, "Payload is invalid");
       });
     });
+    describe("stringToSign method", ()=>{
+      it("should create valid string to sign", ()=>{
+        const params = {
+          requestDate: "20160427T025932Z",
+          region: "east-1",
+          serviceId: "rdb",
+          canonicalRequest: "GET\n/\nAction=CreateDBSecurityGroup&DBSecurityGroupDescription=%E3%83%86%E3%82%B9%E3%83%88%E3%83%95%E3%82%A1%E3%82%A4%E3%82%A2%E3%82%A6%E3%82%A9%E3%83%BC%E3%83%AB&DBSecurityGroupName=test-fire-wall&NiftyAvailabilityZone=east-11\nhost:rdb.jp-east-1.api.cloud.nifty.com\nx-nifty-date:20160427T025932Z\n\nhost;x-nifty-date\ne3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+        };
+        const expect = "NIFTY4-HMAC-SHA256\n20160427T025932Z\n20160427/east-1/rdb/nifty4_request\n0342e5ade7ccb557f1d64c5e8a64f5beab49016c5675aca13cb285d8979cf8a0";
+        const stringToSign = v4.stringToSign(params);
+        assert.equal(expect, stringToSign, "stringToSign is invalid");
+      });
+    });
   });
 });
