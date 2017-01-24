4fe7d75f4fe7b2731a34af8a2e8d18a81ba58dd6
okawa.yuki <okawa.yuki@nifty.co.jp>
2017-01-24 02:55:37 +0000

    Added superagent-proxy

--- example/v4_describeDBInstances.js
+++ example/v4_describeDBInstances.js
@@ -5,13 +5,14 @@ const NiftyCloud = require("../lib/niftycloud.js");
 const niftyCloud = new NiftyCloud(
   "YOUR_ACCESS_KEY",
   "YOUR_SECRET_ACCESS_KEY",
-  "https://rdb.jp-east-1.api.cloud.nifty.com"
+  "https://rdb.jp-east-1.api.cloud.nifty.com",
+  "http://example.com:8080" //proxy server
 );
 
 niftyCloud.V4.get("/", {}, {
   Action: "DescribeDBInstances",
 }, "rdb", "east-1").then((res)=>{
-  console.log("res:" + JSON.stringify(res));
+  console.log(res.text);
 }).catch((err)=>{
   if (err instanceof niftyCloud.Errors.ApiError) {
     console.log("err:" + err);
