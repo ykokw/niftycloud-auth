e8676e774db5b5e7c522c8d01ce5bbfc01f5f175
okawa.yuki <okawa.yuki@nifty.co.jp>
2017-02-09 08:30:24 +0000

    Fixed to separate required arguments from optionals

--- test/signature/client.js
+++ test/signature/client.js
@@ -43,53 +43,67 @@ describe("Client class", ()=>{
       assert.equal(cli.proxy , "");
     });
   });
-  //describe("sendRequest method", ()=>{
-  //  before(()=>{
-  //    nock(endpoint).get("/api/validXmlResponse")
-  //                  .times(2)
-  //                  .replyWithFile(200,
-  //                    "./test/mock/validResponse.xml",
-  //                    {
-  //                      "Content-Type":"application/xml"
-  //                    });
+  describe("sendRequest method", ()=>{
+    const client = new Client(
+      "12345678901234567890",
+      "1234567890abcdefghijklmnopqrstuvwxyzABCD",
+      endpoint
+    );
+    before(()=>{
+      nock(endpoint).get("/api/validXmlResponse")
+                    .times(2)
+                    .replyWithFile(200,
+                      "./test/mock/validResponse.xml",
+                      {
+                        "Content-Type":"application/xml"
+                      });
 
-  //    nock(endpoint).get("/api/validJsonResponse")
-  //                  .times(2)
-  //                  .reply(200, {"result":"ok"});
+      nock(endpoint).get("/api/validJsonResponse")
+                    .times(2)
+                    .reply(200, {"result":"ok"});
 
-  //    nock(endpoint).get("/api/validStringResponse")
-  //                  .times(2)
-  //                  .reply(200, "ok");
+      nock(endpoint).get("/api/validStringResponse")
+                    .times(2)
+                    .reply(200, "ok");
 
-  //    nock(endpoint).get("/api/validEmptyResponse")
-  //                  .times(2)
-  //                  .reply(200, {"result":"ok"});
+      nock(endpoint).get("/api/validEmptyResponse")
+                    .times(2)
+                    .reply(200, {"result":"ok"});
 
-  //    nock(endpoint).get("/api/errorXmlResponse")
-  //                  .times(2)
-  //                  .replyWithFile(400,
-  //                    "./test/mock/errorResponse.xml",
-  //                    {
-  //                      "Content-Type":"application/xml"
-  //                    });
+      nock(endpoint).get("/api/errorXmlResponse")
+                    .times(2)
+                    .replyWithFile(400,
+                      "./test/mock/errorResponse.xml",
+                      {
+                        "Content-Type":"application/xml"
+                      });
 
-  //    nock(endpoint).get("/api/errorJsonResponse")
-  //                  .reply(400, {"error":"k"});
-  //  });
-  //  it("shoud return valid xml response as Object in callback", (next)=>{
-  //    const params = {
-  //      method: "GET",
-  //      url: url.parse(endpoint),
-  //    };
-  //  });
-  //  it("shoud return valid xml response as Object in promise", (next)=>{});
-  //  it("shoud return valid json response as Object in callback", (next)=>{});
-  //  it("shoud return valid json response as Object in promise", (next)=>{});
-  //  it("shoud return api error response as Object in promise", (next)=>{});
-  //  it("shoud return api error response as Object in promise", (next)=>{});
-  //  it("shoud return parse response error response as Object in promise", (next)=>{});
-  //  it("shoud return parse response error response as Object in promise", (next)=>{});
-  //  it("shoud return invalid parameters error response as Object in promise", (next)=>{});
-  //  it("shoud return invalid parameters error response as Object in promise", (next)=>{});
-  //});
+      nock(endpoint).get("/api/errorJsonResponse")
+                    .reply(400, {"error":"k"});
+    });
+    it.only("shoud return valid xml response as Object in callback", (next)=>{
+      const params = {
+        cb    : (err, res)=>{
+          const expectResponseXml = fs.readFileSync("./test/mock/validResponse.xml");
+          parseString(expectResponseXml, (parseErr, result)=>{
+            assert(result !== null);
+            assert(err === null);
+            assert(parseErr === null);
+            assert.deepEqual(res, result, "response didn't match");
+            next();
+          });
+        }
+      };
+      client.sendRequest("get", "/api/validXmlResponse", params);
+    });
+    //it("shoud return valid xml response as Object in promise", (next)=>{});
+    //it("shoud return valid json response as Object in callback", (next)=>{});
+    //it("shoud return valid json response as Object in promise", (next)=>{});
+    //it("shoud return api error response as Object in promise", (next)=>{});
+    //it("shoud return api error response as Object in promise", (next)=>{});
+    //it("shoud return parse response error response as Object in promise", (next)=>{});
+    //it("shoud return parse response error response as Object in promise", (next)=>{});
+    //it("shoud return invalid parameters error response as Object in promise", (next)=>{});
+    //it("shoud return invalid parameters error response as Object in promise", (next)=>{});
+  });
 });
