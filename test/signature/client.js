1f3508430cbfa5d0256c39e48b55208bc4166d89
okawa.yuki <okawa.yuki@nifty.co.jp>
2017-02-10 06:56:16 +0000

    Fixed error object to use make-error module

--- test/signature/client.js
+++ test/signature/client.js
@@ -68,7 +68,15 @@ describe("Client class", ()=>{
 
       nock(endpoint).get("/api/validEmptyResponse")
                     .times(2)
-                    .reply(200, {"result":"ok"});
+                    .reply(204, null);
+
+      nock(endpoint).get("/api/brokenXmlResponse")
+                    .times(2)
+                    .replyWithFile(400,
+                      "./test/mock/brokenResponse.xml",
+                      {
+                        "Content-Type":"application/xml"
+                      });
 
       nock(endpoint).get("/api/errorXmlResponse")
                     .times(2)
@@ -122,23 +130,58 @@ describe("Client class", ()=>{
         next();
       }).catch(next);
     });
-    it("shoud return api error response as Object in promise", (next)=>{
+    it("shoud return api error response as Object in callback", (next)=>{
       const params = {
         cb    : (err, res)=>{
+          assert.equal(res, null);
+          assert(err instanceof client.ApiError);
+          assert.equal(err.name, "ApiError");
+          assert.equal(err.statusCode, 400);
           const expectResponseXml = fs.readFileSync("./test/mock/errorResponse.xml");
           parseString(expectResponseXml, (parseErr, result)=>{
-            assert(parseErr === null);
-            assert.equal(err.status, 400, "response didn't match");
-            assert.deepEqual(err.response, result, "response didn't match");
+            assert.equal(err.errorCode, result.Response.Errors[0].Error[0].Code[0]);
+            assert.equal(err.message, result.Response.Errors[0].Error[0].Message[0]);
             next();
           });
         }
       };
       client.sendRequest("get", "/api/errorXmlResponse", params);
     });
-    //it("shoud return api error response as Object in promise", (next)=>{});
-    //it("shoud return parse response error response as Object in promise", (next)=>{});
-    //it("shoud return parse response error response as Object in promise", (next)=>{});
+    it("shoud return api error response as Object in promise", (next)=>{
+      client.sendRequest("get", "/api/errorXmlResponse").then((res)=>{}).catch((err)=>{
+        assert(err instanceof client.ApiError);
+        assert.equal(err.name, "ApiError");
+        assert.equal(err.statusCode, 400);
+        const expectResponseXml = fs.readFileSync("./test/mock/errorResponse.xml");
+        parseString(expectResponseXml, (parseErr, result)=>{
+          assert.equal(err.errorCode, result.Response.Errors[0].Error[0].Code[0]);
+          assert.equal(err.message, result.Response.Errors[0].Error[0].Message[0]);
+          next();
+        });
+      });
+    });
+    it("shoud return parse response error response as Object in callback", (next)=>{
+      const params = {
+        cb    : (err, res)=>{
+          assert.equal(res, null);
+          assert(err instanceof client.ParseResponseError);
+          assert.equal(err.name, "ParseResponseError");
+          assert.equal(err.message, "Response data is broken");
+          assert.equal(err.statusCode, 400);
+          next();
+        }
+      };
+      client.sendRequest("get", "/api/brokenXmlResponse", params);
+    });
+    it("shoud return parse response error response as Object in promise", (next)=>{
+      client.sendRequest("get", "/api/brokenXmlResponse").then((res)=>{}).catch((err)=>{
+        assert(err instanceof client.ParseResponseError);
+        assert.equal(err.name, "ParseResponseError");
+        assert.equal(err.message, "Response data is broken");
+        assert.equal(err.statusCode, 400);
+        next();
+      });
+    });
     //it("shoud return invalid parameters error response as Object in promise", (next)=>{});
     //it("shoud return invalid parameters error response as Object in promise", (next)=>{});
   });
