a9ceea2e88236d8002c655788d1863f75c443f7a
ykokw <ykokw@example.com>
2017-02-17 18:10:48 +0900

    Fixed to use power-assert

--- test/signature/v4.js
+++ test/signature/v4.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const assert      = require("chai").assert;
+const assert      = require("power-assert");
 const url         = require("url");
 const fs          = require("fs");
 const qs          = require("qs");
