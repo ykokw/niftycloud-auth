126ba7851b8f787e3744227ff2d7dc68e1da9de7
ykokw <ykokw@example.com>
2017-02-17 21:30:43 +0900

    Fixed example of object storage

--- example/v3_getObject.js
+++ example/v3_getObject.js
@@ -2,7 +2,7 @@
 
 const NiftyCloud = require("../lib/niftycloud.js");
 
-const niftyCloud = new NiftyCloud(
+const v3 = new NiftyCloud.V3(
   "YOUR_ACCESS_KEY",
   "YOUR_SECRET_ACCESS_KEY",
   "https://jp-east-2.os.cloud.nifty.com"
@@ -10,14 +10,14 @@ const niftyCloud = new NiftyCloud(
 
 const fs = require("fs");
 
-niftyCloud.V3.get("/first-bucket/script.png", {
-  "Content-Type":"image/png"
-}, null).then((res)=>{
+v3.get("/first-bucket/niftycloud.png", {
+  header: {
+    "Content-Type":"image/png"
+  },
+}).then((res)=>{
   //If the Content-Type is not "application/xml" in response headers, you can get buffer response from response.body property 
-  const image = fs.writeFileSync("./script_get.png", res.body);
+  const image = fs.writeFileSync("./niftycloud_get.png", res.body);
   console.log("res:" + res.status);
 }).catch((err)=>{
-  if (err instanceof niftyCloud.Errors.ApiError) {
-    console.log("err:" + JSON.stringify(err));
-  }
+  console.log(err);
 });
