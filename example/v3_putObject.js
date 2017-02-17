9508a2b0985af3a70b85f196a72ced2c4f6719d9
ykokw <ykokw@example.com>
2017-02-17 21:33:21 +0900

    Fixed example

--- example/v3_putObject.js
+++ example/v3_putObject.js
@@ -4,9 +4,9 @@ const crypto = require("crypto");
 
 const NiftyCloud = require("../lib/niftycloud.js");
 
+const v3 = new NiftyCloud.V3(
   "YOUR_ACCESS_KEY",
   "YOUR_SECRET_ACCESS_KEY",
-const v3 = new NiftyCloud.V3(
   "https://jp-east-2.os.cloud.nifty.com"
 );
 
