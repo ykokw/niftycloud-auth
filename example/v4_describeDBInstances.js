f23834cdb5d8ab812b3485df0c169aacfb194fa5
ykokw <ykokw@example.com>
2017-02-17 21:41:13 +0900

    Fixed v4_describeDBInstance.js

--- example/v4_describeDBInstances.js
+++ example/v4_describeDBInstances.js
@@ -2,21 +2,20 @@
 
 const NiftyCloud = require("../lib/niftycloud.js");
 
-const niftyCloud = new NiftyCloud(
+const v4 = new NiftyCloud.V4(
   "YOUR_ACCESS_KEY",
   "YOUR_SECRET_ACCESS_KEY",
   "https://rdb.jp-east-1.api.cloud.nifty.com",
   "http://example.com:8080" //proxy server
 );
 
-niftyCloud.V4.get("/", {}, {
-  Action: "DescribeDBInstances",
-}, "rdb", "east-1").then((res)=>{
-  console.log(res.text);
-}).catch((err)=>{
-  if (err instanceof niftyCloud.Errors.ApiError) {
-    console.log("err:" + err);
+v4.get("/", "rdb", "east-1", {
+  query: {
+    Action: "DescribeDBInstances"
   }
-  console.log("err:" + err);
+}).then((res)=>{
+  console.log(res.body);
+}).catch((err)=>{
+  console.log(err);
 });
 
