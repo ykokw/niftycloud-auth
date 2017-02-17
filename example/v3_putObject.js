443ff304aa187d855c215ff1fb8fdea003206ad4
ykokw <ykokw@example.com>
2017-02-17 21:24:32 +0900

    Fixed v3_putObject.js

--- example/v3_putObject.js
+++ example/v3_putObject.js
@@ -1,22 +1,29 @@
 "use strict";
 
+const crypto = require("crypto");
+
 const NiftyCloud = require("../lib/niftycloud.js");
 
-const niftyCloud = new NiftyCloud(
   "YOUR_ACCESS_KEY",
   "YOUR_SECRET_ACCESS_KEY",
+const v3 = new NiftyCloud.V3(
   "https://jp-east-2.os.cloud.nifty.com"
 );
 
 const fs = require("fs");
 
-const image = fs.readFileSync("./script.png"); // you must create script.png at current path
-niftyCloud.V3.put("/first-bucket/script.png", { // you must create first-bucket before run this example
-  "Content-Type":"image/png"
-}, null, image).then((res)=>{
+const image = fs.readFileSync("./niftycloud.png"); // you must create niftycloud.png at current path
+const md5hash = crypto.createHash('md5');
+md5hash.update(image);
+
+v3.put("/first-bucket/niftycloud.png", { // you must create first-bucket before run this example
+  header: {
+    "Content-Type":"image/png",
+    "Content-MD5" : md5hash.digest('base64')
+  },
+  body: image
+}).then((res)=>{
   console.log("res:" + res.status);
 }).catch((err)=>{
-  if (err instanceof niftyCloud.Errors.ApiError) {
-    console.log("err:" + JSON.stringify(err));
-  }
+  console.log(err);
 });
