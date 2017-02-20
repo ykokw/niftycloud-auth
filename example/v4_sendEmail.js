178772e37721222898747f6b312ebf7afe81121a
ykokw <ykokw@example.com>
2017-02-20 21:01:47 +0900

    Updated Readme

--- example/v4_sendEmail.js
+++ example/v4_sendEmail.js
@@ -12,8 +12,8 @@ v4.post("/", "east-1", "email", {
   body: {
     "Action":"SendEmail",
     "Version":"2010-12-01",
-    "Destination.ToAddresses.member.1": "0k4w4.yuk1@gmail.com",
-    "Source": "okawa.yuki@nifty.co.jp",
+    "Destination.ToAddresses.member.1": "destination.address@example.com",
+    "Source": "source.address@example.com",
     "Message.Subject.Data": "testFromApi",
     "Message.Body.Text.Data": "testFromApi"
   }
