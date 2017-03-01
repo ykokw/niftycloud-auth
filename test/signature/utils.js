880fdcac14d174d3023a07ca0a3449f85121ce2b
okawa.yuki <okawa.yuki@nifty.co.jp>
2017-03-01 04:25:24 +0000

    Fixed test

--- test/signature/utils.js
+++ test/signature/utils.js
@@ -3,20 +3,30 @@
 const assert      = require("chai").assert;
 const Utils = require("../../lib/signature/utils");
 
-describe("Signature", ()=>{
-  describe("utils library", ()=>{
+describe.only("utils library", ()=>{
+  describe("canonicalQueryString method", ()=>{
     it("should return empty query string with empty queries",()=>{
       assert.equal(Utils.canonicalQueryString({}), "");
-    });
-    it("should return empty header string with empty headers",()=>{
-      assert.equal(Utils.canonicalHeaderString({}), "");
     });
     it("should return empty query string with null",()=>{
       assert.equal(Utils.canonicalQueryString(null), "");
     });
+    it("should return valid query string", ()=>{
+      assert.equal(Utils.canonicalQueryString({key: "value"}), "key=value");
+    });
+    it("should return valid query string that query object include boolean value", ()=>{
+      assert.equal(Utils.canonicalQueryString({"boolean": true}), "boolean=true");
+    });
+  });
+  describe("canonicalHeaderString method", ()=>{
+    it("should return empty header string with empty headers",()=>{
+      assert.equal(Utils.canonicalHeaderString({}), "");
+    });
     it("should return empty header string with null",()=>{
       assert.equal(Utils.canonicalHeaderString(null), "");
     });
+  });
+  describe("fixedEncodeURIComponent method", ()=>{
     it("should return encoded string with url component charactors",()=>{
       assert.equal(Utils.fixedEncodeURIComponent("!'()*"), "%21%27%28%29%2a");
     });
